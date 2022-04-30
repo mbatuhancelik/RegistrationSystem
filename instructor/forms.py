2a0fa66e2b21007c801c6f62654d95c379b9cd04
ersoykadir <eersooy61@gmail.com>
2022-04-30 17:14:36 +0300

    view classrooms cleanup

--- instructor/forms.py
+++ instructor/forms.py
@@ -5,7 +5,4 @@ studentFormFields = ["username", "email", "password", "name", "surname", "depart
 classroomFormFields = ["classroom_id", "campus", "capacity"]
 
 class classroomTimeFilterForm(forms.Form):
-    # classroom_id = forms.CharField(widget=forms.TextInput())
-    # campus = forms.CharField(widget=forms.TextInput())
-    # capacity = forms.IntegerField()
     timeslot = forms.IntegerField()
--- instructor/views.py
+++ instructor/views.py
@@ -10,39 +10,32 @@ def index(req):
         req.session.flush()
         return HttpResponseRedirect('../login?unAuth=False') 
     
-    isFailed=req.GET.get("fail",False)
-    print (isFailed)
+    # Check if timeslot input is valid
+    isValid=req.GET.get("fail",False)
+
     classroom = forms.classroomTimeFilterForm()
-    return render(req,'homepage_instr.html',{"form":classroom,"action_fail":isFailed})
+    return render(req,'homepage_instr.html',{"form":classroom,"action_fail":isValid})
 
 def viewClassrooms(req):
 
     ts = req.GET.get('timeslot')
 
-    if ts != None:
-        result = run_statement(f"""
-        SELECT classroom.classroom_id, classroom.campus, classroom.capacity 
-        FROM classroom where classroom_id not in (
-        select classroom.classroom_id from classroom inner join location on classroom.classroom_id = location.classroom_id and location.timeslot= {ts} );""")
-    else:
-        # Get classrooms that are available, not reserved by a course
-
-        #this shows all classrooms for now
-        # we can modify this such that it only shows all available classrooms
-        result = run_statement( """
-        SELECT classroom.classroom_id, classroom.campus, classroom.capacity, timeslot 
-        FROM classroom left join location on classroom.classroom_id = location.classroom_id ;""")
+    # Select Available Classrooms for the given timeslot 
+    result = run_statement(f"""
+    SELECT classroom.classroom_id, classroom.campus, classroom.capacity 
+    FROM classroom where classroom_id not in (
+    select classroom.classroom_id from classroom inner join location on classroom.classroom_id = location.classroom_id and location.timeslot= {ts} );""")
 
     return render(req,'viewClassrooms.html',{"results":result})
 
 def filterClassrooms(req):
-    print(req.POST['timeslot'])
-    if req.POST['timeslot'] == '':
+
+    if req.POST['timeslot'] == '':# Timeslot empty!
         ts = -1
     else:
         ts = int(req.POST['timeslot'])
 
-    if ts < 1 or ts > 10:
+    if ts < 1 or ts > 10:# Timeslot is invalid!
         return HttpResponseRedirect('.?fail=true')
     else:
 
